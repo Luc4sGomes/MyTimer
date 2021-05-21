0e2cd1b308d751a7f07cafb443b77e29d7cdee07
lucas <lucasgominhaa448@gmail.com>
2021-05-21 12:09:45 -0300

    added new function startClock in script.js

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,18 +2,29 @@ const clock = document.querySelector('.clock');
 const startButton = document.querySelector('.start');
 const pauseButton = document.querySelector('.pause');
 const resetButton = document.querySelector('.reset');
+let seconds = 0;
 
+function startClock(){ //starts the timer
+  const timer = setInterval(function(){
+    seconds ++;
+    clock.innerHTML = getTimeSeconds(seconds); // write in clock the seconds formated
 
-function getTimeSeconds(seconds){ //returns the seconds formated
+  },1000);
+}
+
+function getTimeSeconds(seconds){ //return the date formated
   const data = new Date(seconds * 1000);
   return data.toLocaleTimeString('pt-BR',{
     hour12: false,
-    timeZone: 'GMT'
+    timeZone: 'UTC'
   });
-
 }
 
+console.log(getTimeSeconds(10));
 
 startButton.addEventListener('click',function(event){
-
+  startClock();
 });
+
+
+
