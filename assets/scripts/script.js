a25eda95468427cbaaf21c1cfaf1cbe389468570
lucas <lucasgominhaa448@gmail.com>
2021-05-21 10:54:59 -0300

    :tada: added initial commit

---
+++ assets/scripts/script.js
@@ -0,0 +1,9 @@
+const clock = document.querySelector('.clock');
+const startButton = document.querySelector('.start');
+const pauseButton = document.querySelector('.pause');
+const resetButton = document.querySelector('.reset');
+
+
+startButton.addEventListener('click',function(event){
+
+});
